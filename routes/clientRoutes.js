6f0f726a78e649840f4af8f93a0cd87998af3e56
Bcardoso2 <38514089+Bcardoso2@users.noreply.github.com>
2025-06-04 09:39:24 -0300

    Update clientRoutes.js

--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -1,12 +1,15 @@
- 
 const express = require('express');
 const router = express.Router();
 const clientController = require('../controllers/clientController');
 
-router.get('/', clientController.getClients);
+// Rotas existentes
+router.get('/', clientController.getClients); // Supondo que esta seja a getAllClients
 router.get('/:id', clientController.getClientById);
 router.post('/', clientController.createClient);
 router.put('/:id', clientController.updateClient);
 router.delete('/:id', clientController.deleteClient);
+
+// NOVA ROTA ADICIONADA
+router.get('/:clientId/sales', clientController.getClientSalesHistory);
 
 module.exports = router;
